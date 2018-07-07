4ae9f8f6bcf7de56e550f98ebd9e8cf4c1b5e225
Alex Arvedahl <alex.arvedahl@gmail.com>
2018-07-07 23:36:22 +0200

    Added Pattern matching examples

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,22 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+bookinfo book = "Your book is called  " ++ book
+
+bmiTell weight height = bmiInfo (weight/height)
+
+bmiInfo bmi
+  | bmi <= skinny = "You are underweight"
+  | bmi <= normal = "You are normal"
+  | bmi <= fat = "You are fat"
+  | otherwise = "You are a whale"
+  where skinny = 18.5
+        normal = 25
+        fat = 30
+
+
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r ^ 2
+  in sideArea + 2 * topArea
